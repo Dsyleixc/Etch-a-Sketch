76bd12cb82c5b8472e99cb79eb24d0412fd589ed
Aaron <102453885+Dsyleixc@users.noreply.github.com>
2025-02-26 22:30:54 -0500

    reset button functionality works

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 const container = document.querySelector('.container');
 const containerWidth = container.clientWidth;
 const input = document.querySelector('.grid-size');
-const reset = document.querySelector('.reset');
+const resetBtn = document.querySelector('.reset');
 const changeBtn = document.querySelector('.change-grid');
 
 console.log(containerWidth);
@@ -16,6 +16,7 @@ const createBox = function (boxWidth) {
    // Style and size box
    box.style.width = `${boxWidth}px`;
    box.style.height = `${boxWidth}px`;
+   box.classList.add('box');
 
    // Add box in to container
    container.appendChild(box);
@@ -33,8 +34,15 @@ const createGrid = function (size) {
 };
 
 // Add box hover functionality
-const boxHover = container.addEventListener('mouseover', function (e) {
+container.addEventListener('mouseover', function (e) {
    e.target.classList.add('box-hovered');
+});
+
+resetBtn.addEventListener('click', function () {
+   const boxes = document.querySelectorAll('.box');
+   boxes.forEach(function (box) {
+      box.classList.remove('box-hovered');
+   });
 });
 
 createGrid(16);
